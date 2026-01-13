a43e4f32875cafcf2a5f4c0b9367e8d023f1455f
mehrbawan <m.bano.awan@gmail.com>
2026-01-13 15:21:24 -0500

    nextauth and secret fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -33,6 +33,10 @@ const handler = NextAuth({
   pages: {
     signIn: "/login",
   },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
